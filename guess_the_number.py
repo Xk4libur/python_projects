b5788142ef4e37767a9e24a6974200f00bfbbd22
Xk4libur <xk4libur@gmail.com>
2025-06-27 10:34:09 +0100

    Update guess_the_number.py

--- guess_the_number.py
+++ guess_the_number.py
@@ -5,7 +5,7 @@ secret_num = random.randint(1,20)
 attempt = 0
 
 while attempt != secret_num:
-    user_num = int(input("Put a number: "))
+    user_num = int(input("Put a number between 1 and 20: "))
 
     if user_num < secret_num:
         print("\nToo low.\n")
