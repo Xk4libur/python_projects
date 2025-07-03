f543db5e1f1e259caaf28ebe06163f2a798f8d14
Xk4libur <xk4libur@gmail.com>
2025-07-03 23:23:57 +0100

    Update ppo_python_file_2.py

---
+++ ppo_python_file_2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+# Script with decorator
+
+def decorator(func):
+    def modified_function():
+        print(f"\n- Pre-function execution\n")
+        func()
+        print(f"\n- Post-function execution\n")
+    return modified_function
+
+@decorator
+def install():
+    print(f"\n[+] Installing package...\n")
+
+install()
